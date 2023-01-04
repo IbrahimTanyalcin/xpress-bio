7b72eba0ade49b8f98428fc4976af39897620a68
Ibrahim Tanyalcin <8837895+IbrahimTanyalcin@users.noreply.github.com>
2023-01-04 10:36:40 +0100

    make docker app-name dynamic

--- src/docker-web-app.sh
+++ src/docker-web-app.sh
@@ -68,7 +68,7 @@ iter() {
                 > ^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^
 EOL
             echo 'The fish does not think. Or does he not!!!?' > test-text.txt && \
-                docker cp test-text.txt web-test:app/src/public/assets/;
+                docker cp test-text.txt "${appName}":app/src/public/assets/;
             dx-download-all-inputs && \
                 while read -r -d $'\0'; do 
                     docker cp "$REPLY" "${appName}":app/src/public/assets/bam/;
@@ -88,10 +88,10 @@ EOL
 fixedAssets() {
     return
     #these are fixed reference files
-    ####docker cp ./GCF_003668045.3_CriGri-PICRH-1.0_genomic.fasta web-test:app/src/public/assets/fa/;
-    ####docker cp ./GCF_003668045.3_CriGri-PICRH-1.0_genomic.fasta.fai web-test:app/src/public/assets/fai/;
+    ####docker cp ./GCF_003668045.3_CriGri-PICRH-1.0_genomic.fasta "${appName}":app/src/public/assets/fa/;
+    ####docker cp ./GCF_003668045.3_CriGri-PICRH-1.0_genomic.fasta.fai "${appName}":app/src/public/assets/fai/;
     #these are fixed assets that are optional
     #they will be switched with inputs from the user
-    ####docker cp ./GCF_DHFR-gB-C35_S19_L001_merged.bam web-test:app/src/public/assets/bam/;
-    ####docker cp ./GCF_DHFR-gB-C35_S19_L001_merged.bam.bai web-test:app/src/public/assets/bai/;
+    ####docker cp ./GCF_DHFR-gB-C35_S19_L001_merged.bam "${appName}":app/src/public/assets/bam/;
+    ####docker cp ./GCF_DHFR-gB-C35_S19_L001_merged.bam.bai "${appName}":app/src/public/assets/bai/;
 }
