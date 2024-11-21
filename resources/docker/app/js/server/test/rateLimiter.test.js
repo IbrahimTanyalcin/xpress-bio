06c495cb08ce69ad944dfd9ece9075f316a15af5
Ibrahim Tanyalcin <8837895+IbrahimTanyalcin@users.noreply.github.com>
2024-11-21 09:33:50 +0100

    save progress-6 ratelimit complete

--- resources/docker/app/js/server/test/rateLimiter.test.js
+++ resources/docker/app/js/server/test/rateLimiter.test.js
@@ -1,5 +1,6 @@
 const 
     {setTimeout:awaitableTimeout} = require("node:timers/promises"),
+    genHexStr = require("../../genHexStr.js"),
     timeout = 10000,
     mockInfo = {
         serverConf: {
@@ -57,13 +58,13 @@ describe(`testing websocket rateLimiter`, () => {
         await Promise.all([...limiters.values()].map(limiter => limiter.terminate()))
         cache.end();
         //wait for remaining memcache operations if any
-        await awaitableTimeout(1000, void(0));
+        await awaitableTimeout(500, void(0));
     }, timeout)
 
     test("general use, are synchronous memcached calls eventually adding up?", async () => {
         expect.assertions(1);
         const
-            interval =  1000,
+            interval =  250,
             checkDelay = 250,
             wait = interval + checkDelay,
             limiter = new Limiter(cache, oRoute, {interval});
@@ -80,7 +81,7 @@ describe(`testing websocket rateLimiter`, () => {
     test("does a higher limit work?", async () => {
         expect.assertions(1);
         const
-            interval =  1000,
+            interval =  250,
             checkDelay = 250,
             wait = interval + checkDelay,
             oRouteClone = structuredClone(oRoute);
@@ -96,6 +97,92 @@ describe(`testing websocket rateLimiter`, () => {
         expect(limiter.trigger(o1)).toBe(void(0));
     }, timeout)
 
-    //test step, timeout, global, passing a non object should throw
+    test("does a higher limit work?", async () => {
+        expect.assertions(1);
+        const
+            interval =  250,
+            checkDelay = 250,
+            wait = interval + checkDelay,
+            oRouteClone = structuredClone(oRoute);
+        oRouteClone.limits[0].limit = 2000;
+        oRouteClone.limits[0].step = 2100;
+        const limiter = new Limiter(cache, oRouteClone, {interval});
+        limiters.add(limiter);
+        const o1 = {};
+        await awaitableTimeout(wait);
+        /* 
+            the weak bucket size of ratelimiter is shared and is 20, 
+            normally a single limiter.trigger(o1) would suffice but
+            since weak buckets are shared, it will return the previous
+            objects
+        */
+        for(let i = 0; i < 20; ++i){
+            limiter.trigger(o1)
+        }
+        await awaitableTimeout(wait);
+        expect(limiter.trigger(o1)).toBe(o1);
+    }, timeout)
 
+    test("does timeout work?", async () => {
+        expect.assertions(2);
+        const
+            interval =  250,
+            checkDelay = 250,
+            wait = interval + checkDelay,
+            oRouteClone = structuredClone(oRoute);
+        oRouteClone.limits[0].timeout = 2; //2s
+        const limiter = new Limiter(cache, oRouteClone, {interval});
+        limiters.add(limiter);
+        const o1 = {};
+        await awaitableTimeout(wait);
+        for(let i = 0; i < 1000; ++i){
+            limiter.trigger(o1)
+        }
+        await awaitableTimeout(wait);
+        expect(limiter.trigger(o1)).toBe(o1);
+        await awaitableTimeout(1500); //1500 + 500 + 500 = 2500ms > 2s, timeout should have reset by then
+        expect(limiter.trigger(o1)).toBe(void(0));
+    }, timeout)
+
+    test("does global keys work?", async () => {
+        expect.assertions(2);
+        const
+            interval =  250,
+            checkDelay = 250,
+            wait = interval + checkDelay,
+            oRouteClone = structuredClone(oRoute),
+            hLimit = (oRouteClone.limits[0].limit / 2 | 0) + 1;
+        oRouteClone.limits[0].key = `ratelimiter-test-global-${genHexStr()}`;
+        oRouteClone.limits[0].global = true;
+        const 
+            limiter = new Limiter(cache, oRouteClone, {interval}),
+            limiter2 = new Limiter(cache, oRouteClone, {interval});
+        limiters.add(limiter);
+        limiters.add(limiter2);
+        const o1 = {};
+        await awaitableTimeout(wait);
+        for(let i = 0; i < hLimit; ++i){ //a bit less than 30
+            limiter.trigger(o1)
+        }
+        await awaitableTimeout(wait);
+        expect(limiter.trigger(o1)).toBe(void(0));
+        for(let i = 0; i < hLimit; ++i){ //a bit less than 30
+            limiter.trigger(o1)
+        }
+        await awaitableTimeout(wait);
+        expect(limiter2.trigger(o1)).toBe(o1);
+    }, timeout)
+
+    //passing a non object should throw
+    test("general use, are synchronous memcached calls eventually adding up?", async () => {
+        expect.assertions(1);
+        const
+            interval =  250,
+            checkDelay = 250,
+            wait = interval + checkDelay,
+            limiter = new Limiter(cache, oRoute, {interval});
+        limiters.add(limiter);
+        const testString = "string";
+        expect(() => limiter.trigger(testString)).toThrow();
+    }, timeout);
 })
